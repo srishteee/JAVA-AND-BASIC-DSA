ec1df1f62e0ddb182bcea7830eaf550206ded535
Srishteee <srishtinagarkoti.25@gmail.com>
2025-07-21 18:57:16 +0530

    java & dsa

---
+++ src/LinkedList/Basics.java
@@ -0,0 +1,19 @@
+package LinkedList;
+import java.util.*;
+public class Basics {
+    public static class Node {
+        int data ;
+        Node next;
+        // it has these properties
+    }
+    public static class LinkedLists{
+        Node head;
+        Node tail;
+        int size;
+        // taking all the things here as a summary
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
