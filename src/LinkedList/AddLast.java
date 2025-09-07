761384f7126a8d455f3beba1ca05f1106b47eee2
Srishteee <srishtinagarkoti.25@gmail.com>
2025-09-07 23:26:37 +0530

    java & dsa

--- src/LinkedList/AddLast.java
+++ src/LinkedList/AddLast.java
@@ -38,6 +38,55 @@ while(temp !=null){
 }
             System.out.println();
         }
+
+        public void removeFirst(){
+
+            if(size==0){
+                System.out.println("empty list");
+            } else if (size==1) {
+                head=tail=null;
+                size=0;
+            } else {
+                head=head.next;
+                size--;
+            }
+
+        }
+        // this method gives the head
+        public int getFirst(){
+if(size==0){
+    System.out.println("list is empty");
+    return -1;
+} else{
+return head.data;
+}
+        }
+
+        // this method gives the tail
+        public int getLast(){
+            if(size==0){
+                System.out.println("list is empty");
+                return -1;
+            } else{
+           return tail.data;
+            }
+        }
+        // this methods gives the value where it is like where we want the data from
+        public int getAt(int idx) {
+            if (size == 0) {
+                System.out.println("list is empty");
+                return -1;
+            } else if (idx < 0 || idx >= size) {
+                System.out.println("invalid arguments");
+                return -1;
+            } else {
+                Node temp = head;
+   for(int i=0;i<idx;i++){
+temp=temp.next;
+   }
+   return temp.data;
+            }
+        }
         }
 
 
