0df2dc08748793b68d2e00d18a661ce5a016f5c2
Srishteee <srishtinagarkoti.25@gmail.com>
2025-11-10 00:57:35 +0530

    LinkedList implementation

--- src/LinkedList/AddLast.java
+++ src/LinkedList/AddLast.java
@@ -87,6 +87,17 @@ temp=temp.next;
    return temp.data;
             }
         }
+
+        public void addFirst(int val){
+        Node temp = new Node();
+        temp.data=val;
+        temp.next=head;
+        head=temp;
+
+        if(size==0){
+            tail=temp;
+        }
+        size++;
         }
 
 
